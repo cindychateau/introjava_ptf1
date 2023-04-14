5ca30d88bb09771591492ab5eef6e08e3cfe9d3b
Cynthia Castillo <ca.castillo@gmail.com>
2023-04-13 18:07:59 -0600

    Getters y Setters

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,11 +1,11 @@
 class Vehiculo {
 
     /*Variables miembro - Características o atributos de mi objeto */
-    public int anio;
-    public String marca;
-    public String modelo;
-    public String color;
-    public int ruedas;
+    private int anio;
+    private String marca;
+    private String modelo;
+    private String color;
+    private int ruedas;
 
     /*Constructor - Inicializa un objeto */
     public Vehiculo() {
@@ -38,6 +38,57 @@ class Vehiculo {
 
     public void acelera() {
         System.out.println("El vehículo "+this.marca+" está acelerando!");
+    }
+
+    /*
+    A través de los getters y setters vamos a poder modificar y consultar los datos de un objeto
+     */
+    
+    //Getter es el encargado de regresarme un dato
+    public int getAnio() {
+        return this.anio;
+    }
+
+    //Setter es el encargado de asignar un valor a mi atributo
+    /*
+    anio = 2001
+    this = auto1
+    auto1.anio = 2001
+     */
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getRuedas() {
+        return this.ruedas;
+    }
+
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
     }
 
 }
--- VehiculoTest.java
+++ VehiculoTest.java
@@ -4,30 +4,39 @@ class VehiculoTest {
         //Creamos un vehiculo
         ///Inicializamos la instancia
         Vehiculo auto1 = new Vehiculo(); 
-        auto1.anio = 2001;
-        auto1.marca = "Honda";
-        auto1.modelo = "Civic";
-        auto1.color = "blanco";
-        auto1.ruedas = 4;
+        //auto1.anio = 2001;
+        auto1.setAnio(2001);
+        //auto1.marca = "Honda";
+        auto1.setMarca("Honda");
+        //auto1.modelo = "Civic";
+        auto1.setModelo("Civic");
+        //auto1.color = "blanco";
+        auto1.setColor("blanco");
+        //auto1.ruedas = 4;
+        auto1.setRuedas(4);
 
-        System.out.println("Marca auto1:"+ auto1.marca);
+        System.out.println("Marca auto1:"+ auto1.getMarca());
 
         Vehiculo auto2 = new Vehiculo("rojo");
-        auto2.anio = 2018;
-        auto2.marca = "Mazda";
-        auto2.modelo = "CX-3";
-        auto2.ruedas = 4;
+        //auto2.anio = 2018;
+        auto2.setAnio(2018);
+        //auto2.marca = "Mazda";
+        auto2.setMarca("Mazda");
+        //auto2.modelo = "CX-3";
+        auto2.setModelo("CX-3");
+        //auto2.ruedas = 4;
+        auto2.setRuedas(4);
 
-        System.out.println("Marca auto2:" + auto2.marca);
+        System.out.println("Marca auto2:" + auto2.getMarca());
         //System.out.printf("Marca auto2: %s", auto2.marca);
 
         //System.out.println(String.format("Marca auto2: %s", auto2.marca));
 
         Vehiculo auto3 = new Vehiculo("azul", "Tesla");
-        System.out.println("Marca auto3:" + auto3.marca);
+        System.out.println("Marca auto3:" + auto3.getMarca());
 
         Vehiculo bicicleta = new Vehiculo(2);
-        System.out.println("Ruedas de bici:"+bicicleta.ruedas);
+        System.out.println("Ruedas de bici:"+bicicleta.getRuedas());
 
         Vehiculo auto4 = new Vehiculo("gris", "BMW");
 
